49458cf355635981b267c22dd9af2c1df7975860
vitorcostao <vitor.oliveira.rolla@gmail.com>
2025-03-26 09:59:50 -0300

    Algoritmos de ordenação - falta o MergeSort

---
+++ Estruturas/Ordenações/MergeSort.java
@@ -0,0 +1,19 @@
+
+public class MergeSort {
+
+    public static void Imprimir(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Definir dados
+        int[] array = { 56, 12, 73, 98, 114 };
+        int n = array.length;
+
+    }
+}
